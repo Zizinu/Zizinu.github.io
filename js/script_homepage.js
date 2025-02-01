cc2067689de26bfe492ce0ddfc014147eb8dc166
Zizinu <ayrizboelen@gmail.com>
2025-02-01 15:11:26 +0100

    Update script_homepage.js

--- js/script_homepage.js
+++ js/script_homepage.js
@@ -18,7 +18,7 @@ let lastMouseY = -1;
   for (let i = 0; i < 30; i++) {
     let x = random(100, width - 100);
     let y = random(100, height - 100);
-    let imagePath = `/slide_test_1/assets/img/alle_${i + 1}.jpeg`;
+    let imagePath = `/assets/alle_${i + 1}.jpeg`;
     console.log(`Attempting to load image: ${imagePath}`); 
     items.push(new Item(x, y, imagePath));
   }
